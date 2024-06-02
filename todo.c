510ff25640146f91291d5085f6fc6a90beb045c4
Justin Navarro <info@justinnavarro.net>
2024-06-02 10:31:34 -0400

    build the starter window

---
+++ todo.c
@@ -0,0 +1,20 @@
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Todo", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    while(!glfwwindowShould(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
